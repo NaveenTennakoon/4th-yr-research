0e87827af83e910ef20611c51d2ed792ea8501b1
Naveen Tennakoon <naveentennakoon27@gmail.com>
2021-08-25 11:47:56 +0530

    add lstm-i3d fused data generator

--- datagenerator.py
+++ datagenerator.py
@@ -233,6 +233,121 @@ class MultipleInputGenerator(Sequence):
     def data_generation(self, seFlowVideo:pd.Series, seLipVideo:pd.Series):
         return self.__data_generation(seFlowVideo, seLipVideo)
 
+class LSTMI3DInputGenerator(Sequence):
+    """ Read and yields video frames/optical flow and lip frames for Keras.model.fit_generator
+    Generator can be used for multi-threading.
+    Substantial initialization and checks upfront, including one-hot-encoding of labels.
+    This generator is used for joint fusion training
+    """
+
+    def __init__(self, flowPath:str, lipPath:str, batchSize:int, nFrames:int, flowHeight:int, flowWidth:int, \
+        featureLength:int, nFlowChannels:int, liClassesFull:list = None, \
+        shuffle:bool = True):
+        """ Assume directory structure: ... / flowPath / class / videoname / frames.jpg
+                                      : ... / lipPath / class / videoname / frames.jpg
+        """
+
+        'Initialization'
+        self.batchSize = batchSize
+        self.nFrames = nFrames
+        self.flowHeight = flowHeight
+        self.flowWidth = flowWidth
+        self.nFlowChannels = nFlowChannels
+        self.tuFlowXShape = (nFrames, flowHeight, flowWidth, nFlowChannels)
+        self.tuLipXShape = (nFrames, featureLength)
+        self.shuffle = shuffle
+
+         # retrieve all feature files
+        self.dfSamples = pd.DataFrame(sorted(glob.glob(lipPath + "/*/*.npy")), columns=["sPath"])
+        self.nFeatureSamples = len(self.dfSamples)
+        if self.nFeatureSamples == 0: raise ValueError("Found no feature files in " + lipPath)
+        print("Detected %d samples in %s ..." % (self.nFeatureSamples, lipPath))
+
+        # retrieve all videos = frame directories
+        self.flowVideos = pd.DataFrame(sorted(glob.glob(flowPath + "/*/*")), columns=["sFrameDir"])
+        self.nSamples = len(self.flowVideos)
+        if self.nSamples == 0: raise ValueError("Found no frame directories files in " + flowPath)
+        print("Detected %d samples in %s ..." % (self.nSamples, flowPath))
+        if self.nSamples != self.nFeatureSamples : raise ValueError("Directory samples are mismatched")
+
+        # extract (text) labels from flowPath
+        seLabels =  self.flowVideos.sFrameDir.apply(lambda s: s.replace('\\' , '/').split("/")[-2])
+        self.flowVideos.loc[:, "sLabel"] = seLabels
+            
+        # extract unique classes from all detected labels
+        self.liClasses = sorted(list(self.flowVideos.sLabel.unique()))
+
+        # if classes are provided upfront
+        if liClassesFull != None:
+            liClassesFull = sorted(np.unique(liClassesFull))
+            # check detected vs provided classes
+            if set(self.liClasses).issubset(set(liClassesFull)) == False:
+                raise ValueError("Detected classes are NOT subset of provided classes")
+            # use superset of provided classes
+            self.liClasses = liClassesFull
+            
+        self.nClasses = len(self.liClasses)
+
+        # encode labels
+        trLabelEncoder = LabelEncoder()
+        trLabelEncoder.fit(self.liClasses)
+        self.flowVideos.loc[:, "nLabel"] = trLabelEncoder.transform(self.flowVideos.sLabel)
+        
+        self.on_epoch_end()
+        
+        return
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.ceil(self.nSamples / self.batchSize))
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(self.nSamples)
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __getitem__(self, nStep):
+        'Generate one batch of data'
+
+        # Generate indexes of the batch
+        indexes = self.indexes[nStep*self.batchSize:(nStep+1)*self.batchSize]
+
+        # get batch of videos
+        flowVideosBatch = self.flowVideos.loc[indexes, :]
+        dfSamplesBatch = self.dfSamples.loc[indexes, :]
+        batchSize = len(flowVideosBatch)
+
+        # initialize arrays
+        arFlowX = np.empty((batchSize, ) + self.tuFlowXShape, dtype = float)
+        arLipX = np.empty((batchSize, ) + self.tuLipXShape, dtype = float)
+        arY = np.empty((batchSize), dtype = int)
+
+        # Generate data
+        for i in range(batchSize):
+            # generate data for single video
+            arFlowX[i,], arLipX[i,], arY[i] = self.__data_generation(flowVideosBatch.iloc[i,:], dfSamplesBatch.iloc[i,:])
+
+        # onehot the labels
+        return [np.array(arFlowX), np.array(arLipX)], np.array(to_categorical(arY, num_classes=self.nClasses))
+
+    def __data_generation(self, seFlowVideo:pd.Series, seSample:pd.Series):
+        "Returns frames for 1 video, including normalizing & preprocessing"
+       
+        # Get the frames from disc
+        arFlowFrames = files2frames(seFlowVideo.sFrameDir)
+        arLipFeatures = np.load(seSample.sPath)
+
+        # only use the first nFlowChannels (typically 3, but maybe 2 for optical flow)
+        arFlowFrames = arFlowFrames[..., 0:self.nFlowChannels]
+        
+        ar_fFrames = images_normalize(arFlowFrames, self.nFrames, self.flowHeight, self.flowWidth, rescale = True)
+        
+        return ar_fFrames, arLipFeatures, seFlowVideo.nLabel
+
+    def data_generation(self, seFlowVideo:pd.Series, seLipVideo:pd.Series):
+        return self.__data_generation(seFlowVideo, seLipVideo)
+
 class FeaturesGenerator(Sequence):
     """Reads and yields (preprocessed) VGG19 features for Keras.model.fit_generator
     Generator can be used for multi-threading.
