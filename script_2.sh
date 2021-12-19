ebb75cd8aee87215c748c6e5b5283caa1b46103e
sachininayanathara <sach.nayanathara1995@gmail.com>
2021-12-19 16:03:50 +0530

    training starts in antpc

--- script_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /home/antpc/anaconda3/etc/profile.d/conda.sh;
-CUDA_VISIBLE_DEVICES=1;
-conda activate continuous
-cd CSLR
-tzq config/baseline_9.yml train --continue
-conda deactivate
--- script_3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /home/antpc/anaconda3/etc/profile.d/conda.sh;
-CUDA_VISIBLE_DEVICES=1;
-conda activate continuous
-cd CSLR
-tzq config/baseline_10.yml train --continue
-conda deactivate
